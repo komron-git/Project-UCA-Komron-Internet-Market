693ed7a3c6d45a3dca2bcd127d026bcb214d496f
komron-git <komronshodiev2001@gmail.com>
2022-09-18 20:52:17 +0500

    add categiry in db

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True)
+    price =models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.title
+
+class  CartProduct(models.Model):
+
+    user = models.ForeignKey('Customer', verbose_name='Покупатель',on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart',verbose_name='Корзина',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default= 1)#Количество
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+
+
+    def __str__(self):
+        return "Продукт: {} (для корзины)".format(self.product.title)
+
+class Cart(models.Model):
+
+    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
