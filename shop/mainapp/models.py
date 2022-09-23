3ea41d61bbb73afe773a0d3da74cdcff1076e1f2
komron-git <komronshodiev2001@gmail.com>
2022-09-23 13:51:44 +0500

    Create models in app

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+
+User = get_user_model()#Использование user  который указан в settings
+
+#**************
+#1Category
+#2Product
+#3CartProduct
+#4Cart
+#5Order
+#***************
+#6Customer
+#7Specification
 
 class Category(models.Model):
 
@@ -23,7 +37,7 @@ class Product(models.Model):
 class  CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Покупатель',on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart',verbose_name='Корзина',on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина',on_delete=models.CASCADE, related_name='related_products')
     product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default= 1)#Количество
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
@@ -35,3 +49,30 @@ class  CartProduct(models.Model):
 class Cart(models.Model):
 
     owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
+    product = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
+    total_products = models.PositiveIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')#Окончательная цена корзины
+
+    def __str__(self):
+        return str(self.id)
+
+class Customer(models.Model):
+
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    adress = models.CharField(max_length=255, verbose_name='Адрес')
+
+    def __str__(self):
+        return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
+
+class Specification(models.Model):
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
+
+    def __str__(self):
+        return "Характеристики для товара: {}".format(self.name)
+
+
+
